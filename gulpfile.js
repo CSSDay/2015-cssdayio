f4ee4e2de90347f5b5119abde538884a3b1d95b4
Brad Westfall <brad@azpixels.com>
2015-08-13 14:58:18 -0700

    css concatenation and shakeit

--- gulpfile.js
+++ gulpfile.js
@@ -3,55 +3,67 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
-var minifyCss   = require('gulp-minify-css');
+var del         = require('del');
 var rename      = require('gulp-rename');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
 var serve       = require('gulp-serve');
 
-/**
- * Server
- */
+gulp.task('clean-js', function (cb) {
+    del('./build/*');
+    cb();
+});
+
+gulp.task('clean-css', function (cb) {
+    del('./css/*');
+    cb();
+});
+
+gulp.task('clean-sass-vendors', function (cb) {
+    del('./scss/vendors/*');
+    cb();
+});
+
 gulp.task('server', serve({
-  root: ['.'],
-  port: 8080
+    root: ['.'],
+    port: 8080
 }));
 
-/**
- * JS
- */
- gulp.task('js', function() {
-   return gulp.src([
-         './vendor/jquery/dist/jquery.js',
-         './vendor/slick-carousel/slick/slick.js',
-         './src/**/*.js'
-       ])
-     .pipe(concat('build.js'))
-     .pipe(gulp.dest('./build'))
-     // .pipe(uglify())
-     // .pipe(rename('build.min.js'))
-     // .pipe(gulp.dest('./scripts'))
- });
+gulp.task('js', ['clean-js'], function() {
+    return gulp.src([
+            './vendor/jquery/dist/jquery.js',
+            './vendor/slick-carousel/slick/slick.js',
+            './src/**/*.js'
+        ])
+    .pipe(concat('build.js'))
+    .pipe(gulp.dest('./build'))
+    .pipe(uglify())
+    .pipe(rename('build.min.js'))
+    .pipe(gulp.dest('./build'))
+});
 
-/**
- * Sass
- */
-gulp.task('sass', function () {
+gulp.task('sass-convert-partials', ['clean-sass-vendors'], function() {
+    gulp.src('vendor/normalize.css/normalize.css')
+        .pipe(rename('_normalize.scss'))
+        .pipe(gulp.dest('./scss/vendors/'));
+    gulp.src('vendor/slick-carousel/slick/slick.css')
+        .pipe(rename('_slick.scss'))
+        .pipe(gulp.dest('./scss/vendors/'));
+});
+
+gulp.task('sass', ['clean-css', 'sass-convert-partials'], function () {
     return gulp.src('./scss/*.scss')
         .pipe(sass({
             includePaths: ['scss'],
             errLogToConsole: true
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8']))
-        .pipe(gulp.dest('./css'))
-        .pipe(minifyCss({compatibility: 'ie8'}))
-        //.pipe(rename('main.min.css'))
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./vendor/*', './src/**/*.js'], ['js']);
-    gulp.watch(['./scss/**/*.scss', './vendor/normalize.css/normalize.css'], ['sass']);
+    gulp.watch(['./src/**/*.js', './vendor/*.js'], ['js']);
+    gulp.watch(['./scss/**/*.scss', './vendor/*.css'], ['sass']);
 });
 
 gulp.task('default', ['sass', 'js', 'watch', 'server']);
